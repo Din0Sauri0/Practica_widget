38c39e7b0743645a3403a6a38180615ff3aaafc3
Gustavo <gustavo.ovalle.zambrano@gmail.com>
2020-09-07 13:20:30 -0300

    app de encusta sobre microsoft edge. -se ha creado la interfaz grafica. -se han creado variables private para enlazar con los widget. -se han enlazado las variables con los widget. -se han desabilitado algunos widget al momento de inicar la app.

--- app/src/main/java/com/ovalle/practica_widget/MainActivity.java
+++ app/src/main/java/com/ovalle/practica_widget/MainActivity.java
@@ -3,12 +3,51 @@ package com.ovalle.practica_widget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtNombre;
+    private  EditText txtApellido;
+    private Button btnEnviar;
+    private RadioGroup rbUsas;
+    private  RadioGroup rbRecomiendas;
+    private CheckBox cbxNotificaciones;
+    private RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellido = findViewById(R.id.txtApellido);
+        btnEnviar = findViewById(R.id.btnEnviar);
+        rbUsas = findViewById(R.id.rgUsas);
+        rbRecomiendas = findViewById(R.id.rgRecomiendas);
+        cbxNotificaciones = findViewById(R.id.cbxNotificaciones);
+        ratingBar = findViewById(R.id.ratingBar);
+
+        ratingBar.setEnabled(false); //desahabilitar widget en la interfaz al momento de inicar (ratingBar).
+        btnEnviar.setEnabled(false); //desahabilitar widget en la interfaz al momento de iniciar (btnEviar).
+        rbRecomiendas.setEnabled(false);
+
+
+        rbUsas.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.rbSi){
+                    ratingBar.setEnabled(true);
+                    rbRecomiendas.setEnabled(true);
+                }else{
+                    ratingBar.setEnabled(false);
+                    ratingBar.setRating(0);
+                    rbRecomiendas.setEnabled(false);
+                }
+            }
+        });
     }
 }
